9a1955bcb0f1f061a030c68724ffac6807a7832c
p-328 <ram.patel3882323@gmail.com>
2022-08-11 22:27:20 -0400

    Add project files.

---
+++ BST/BST.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    public class BinSTree
+    {
+        public int data;
+        public BinSTree? right;
+        public BinSTree? left;
+
+        public BinSTree(int data)
+        {
+            this.data = data;
+            right = null;
+            left = null;
+        }
+    }
+    
+}
---
+++ BST/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BST;
+
+
+static void Insert(ref BinSTree? root, int data)
+{
+    if (root == null)
+    {
+        BinSTree node = new BinSTree(data);
+        root = node;
+        return;
+    }
+
+    if (root.data > data) Insert(ref root.left, data);
+    else Insert(ref root.right, data);
+}
+
+static void printNodes(BinSTree? root)
+{
+    if (root == null) return;
+    printNodes(root.left);
+    Console.WriteLine(root.data);
+    printNodes(root.right);
+}
+
+static bool Search(BinSTree? root, int val)
+{
+    if (root == null) return false;
+    if (val == root.data) return true;
+    if (val < root.data) Search(root.left, val);
+    else if (val > root.data) Search(root.right, val);
+    return false;
+}
+
+BinSTree? root = new(1);
+for (int i = 0; i < 100; i++)
+{
+    Insert(ref root, i*i*i);
+}
+
+
+Console.WriteLine(Search(root, 8));
